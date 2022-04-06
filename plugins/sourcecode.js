28c9da81fc1bd9488a66121aee3c5f27ed266b83
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-06 15:07:42 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,8 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-Sc Nya Liat Di https://youtu.be/0wz53xj_5hI
-Jgn Lupa Di Subscribe yaa
-`.trim(), '❖ Kanao-Bot', 'Siap Banh🗿', 'Thanks')
+Sc Ori (Enc) : http://github.com/LynnXzy/scbotz-v1
+
+Sc Recode (No Enc) : Chat + Minta Sama Owner Ngab, Gemratis Kok:v
+`.trim(), 'DanzBot', 'Makasih Bg🥀', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
