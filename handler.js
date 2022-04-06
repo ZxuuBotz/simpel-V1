1c4e9cac762b93479d8ab45c42eb03ccb4da937a
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-06 14:45:38 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -660,11 +660,11 @@ global.dfail = (type, m, conn) => {
   let botAdmin = {
     botAdmin: 'Mikir dkit lah bang *BOT NYA BUKAN ADMIN*',
   }[type]
-  if (botAdmin) return conn.sendButton(m.chat, botAdmin, 'Lupa Saya', 'Eh iya ya', 'Nanii', m)
+  if (botAdmin) return conn.sendButton(m.chat, botAdmin, 'Lupa Gua🗿', 'Eh iya Ngab Danz Lupa🗿', 'Nanii', m)
   let unreg = {
-    unreg: `Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar LynXzy.16*`
+    unreg: `Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Danz.16*`
   }[type]
-  if (unreg) return conn.sendButton(m.chat, `Halo kak ${name} Sebelum menggunakan fitur ini Harap Verify Terlebih dahulu`, watermark, 'Verify', `#daftar ${name}.17`, { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `©shirobotz_\nJam ${time}`, itemCount: 2022, thumbnail: fs.readFileSync('./src/RadBotZ.jpg')}}})
+  if (unreg) return conn.sendButton(m.chat, `Halo Dex ${name}!Kalau Mau menggunakan fitur ini Daftar Terlebih dahulu Yah dex,biar masuk ke dalam database *_DanzBot_*`, watermark, '⬡ *_DAFTAR_*', `#daftar ${name}.17`, { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `©shirobotz_\nJam ${time}`, itemCount: 2022, thumbnail: fs.readFileSync('./src/RadBotZ.jpg')}}})
 }
 
 let chalk = require('chalk')
